36292f338ed7b5b2978ab5955302a4ecdbd6c0f5
Elvira <elvi_ac94@hotmail.com>
2020-03-07 22:32:06 +0100

    Add on click create card button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -12,12 +12,20 @@ console.log('>> Ready :)');
 
 
 //DESING COMPARTE
-const shareTop= document.querySelector('.share--top');
-const shareBottom= document.querySelector('.share--bottom');
+const shareTop = document.querySelector('.share--top');
+const shareBottom = document.querySelector('.share--bottom');
+const createButton = document.querySelector('.create-card--button');
+const createCardContainer = document.querySelector('.created-card--container');
 
 function onClickShareTop(){
     shareBottom.classList.toggle('share--bottom--visible');
     shareTop.classList.toggle('share--top--active');
 }
 
+function onClickCreateButton(){
+    createButton.classList.add('create-card--button--active');
+    createCardContainer.classList.add('created-card--container--visible');
+}
+
 shareTop.addEventListener('click', onClickShareTop);
+createButton.addEventListener('click', onClickCreateButton);
