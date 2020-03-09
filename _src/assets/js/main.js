1b32cf8b4b6af8d5df1fec3d560739f27e67da10
maitevillar <sayhello.maite@gmail.com>
2020-03-09 21:04:59 +0100

    with JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,7 +2,79 @@
 
 console.log('hola'); 
 
-// DESGIN FILL
+
+//DESIGN COLOR
+
+const designTop = document.querySelector('.design--top');
+const designBottom = document.querySelector('.design--bottom');
+const inputColorPalette1 = document.querySelector('#color-option1')
+const inputColorPalette2 = document.querySelector('#color-option2')
+const inputColorPalette3 = document.querySelector('#color-option3')
+const cardDecoration = document.querySelector('#image--preview_text')
+const cardIcons = document.querySelector('#image--preview_icons')
+
+function onClickDesignTop(){
+    designBottom.classList.toggle('design--bottom--visible');
+    designTop.classList.toggle('design--top--active');
+}
+
+function changePalette1(){
+    cardDecoration.classList.add('color-palette1');
+    cardIcons.classList.add('icon-palette1');
+    cardDecoration.classList.remove('color-palette2');
+    cardIcons.classList.remove('icon-palette2');
+    cardDecoration.classList.remove('color-palette3');
+    cardIcons.classList.remove('icon-palette3');
+    }
+
+function changePalette2(){
+    cardDecoration.classList.add('color-palette2');
+    cardIcons.classList.add('icon-palette2');
+    cardDecoration.classList.remove('color-palette1');
+    cardIcons.classList.remove('icon-palette1');
+    cardDecoration.classList.remove('color-palette3');
+    cardIcons.classList.remove('icon-palette3');
+    }
+
+function changePalette3(){
+    cardDecoration.classList.add('color-palette3');
+    cardIcons.classList.add('icon-palette3');
+    cardDecoration.classList.remove('color-palette1');
+    cardIcons.classList.remove('icon-palette1');
+    cardDecoration.classList.remove('color-palette2');
+    cardIcons.classList.remove('icon-palette2');
+    }
+
+
+designTop.addEventListener('click', onClickDesignTop);
+inputColorPalette1.addEventListener('change', changePalette1)
+inputColorPalette2.addEventListener('change', changePalette2)
+inputColorPalette3.addEventListener('change', changePalette3)
+
+
+
+//DESIGN SCRIPT
+const fillTop = document.querySelector('.fill--top');
+const fillBottom = document.querySelector('.fill--bottom');
+
+let inputName = document.querySelector('#firstName');
+let inputJob = document.querySelector('#addjob');
+let cardFullName = document.querySelector('.card--full_name');
+let cardFullProfession = document.querySelector('.card--full_profession');
+
+function onClickFillTop(){
+    fillBottom.classList.toggle('fill--bottom--visible');
+    fillTop.classList.toggle('fill--top--active');
+}
+
+function print(evt){
+    if(evt.currentTarget.name === 'firstName') {
+        cardFullName.innerHTML = evt.currentTarget.value
+    }
+    if(evt.currentTarget.name === 'addjob') {
+        cardFullProfession.innerHTML = evt.currentTarget.value
+    }
+};
 
 const FillIconPhone = document.querySelector('#icon1');
 const FillIconEmail = document.querySelector('#icon2');
@@ -32,12 +104,16 @@ if (finalInput===FillInputPhone){
     }
 }
 
+fillTop.addEventListener('click', onClickFillTop);
+inputName.addEventListener('keyup', print);
+inputJob.addEventListener('keyup', print);
 FillInputPhone.addEventListener('keypress', showIcon);
 FillInputEmail.addEventListener('keypress', showIcon);
 FillInputLinkedin.addEventListener('keypress', showIcon);
 FillInputGithub.addEventListener('keypress', showIcon);
 
 //DESING COMPARTE
+
 const shareTop = document.querySelector('.share--top');
 const shareBottom = document.querySelector('.share--bottom');
 const createButton = document.querySelector('.create-card--button');
