1d848cab4caeb6366eabb7985d71a39cdba8530d
Ana Belen Ferreira <ana@pcdeana.localdomain>
2020-03-25 12:37:21 +0100

    changestwitter-share

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -215,7 +215,7 @@ function reset() {
   inputJob.value = '';
   FillInputPhone.value = '';
   FillInputEmail.value = '';
-  FillInputGithub.value ='';  
+  FillInputGithub.value ='';
   FillInputLinkedin.value='';
   cardFullName.innerHTML = 'Nombre Apellidos';
   cardFullProfession.innerHTML = 'Front-end developer';
@@ -225,11 +225,62 @@ function reset() {
     FillIconLinkedin.classList.add('hidden--fill');
     FillIconGithub.classList.add('hidden--fill');
 
-  changePalette1()
-  showIcon(event) 
-  chosenInput()
-
+  changePalette1();
+  showIcon(event) ;
+  chosenInput();
 }
 
 
-resetButton.addEventListener('click', reset)
+resetButton.addEventListener('click', reset);
+
+//js-avatar:
+
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.js__profile-trigger');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
+
+
+/**
+ * Recoge el archivo añadido al campo de tipo "file"
+ * y lo carga en nuestro objeto FileReader para que 
+ * lo convierta a algo con lo que podamos trabajar.
+ * Añade un listener al FR para que ejecute una función
+ * al tener los datos listos
+ * @param {evento} e 
+ */
+function getImage(e){
+  var myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+
+
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
+function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+
+/**
+ * Genera un click automático en nuesto campo de tipo "file"
+ * que está oculto
+ */
+function fakeFileClick() {
+ fileField.click();
+}
+
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
+uploadBtn.addEventListener('click', fakeFileClick);
